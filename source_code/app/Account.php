da5edf4e5ae8b82f156e28595f7eedf435b32384
Luan Nguyen <huuluanpy@gmail.com>
2017-12-25 01:43:40 +0700

    Register Account - Display errors Vietnamese

--- source_code/app/Account.php
+++ source_code/app/Account.php
@@ -2,20 +2,14 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class Account extends Model
 {
-    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+
     protected $fillable = [
-        'name', 'username', 'password', 'status_id'
+         'username', 'password', 'status_id'
     ];
 
     /**
