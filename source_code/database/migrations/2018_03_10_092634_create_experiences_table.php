a3631077d5424181d23ba212812d1ff9252c921b
Luan Nguyen <huuluanpy@gmail.com>
2018-03-10 10:04:16 +0700

    update database

---
+++ source_code/database/migrations/2018_03_10_092634_create_experiences_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',255);
+            $table->string('role',255);
+            $table->date('from');
+            $table->date('to');
+            $table->integer('student_id')->index()->unsigned();
+
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiences');
+    }
+}
